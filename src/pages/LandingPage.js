f66d5eef2233e90be2f2d101b52bca22bfdc237b
Kim Charles <hau.kmdeguzman1@gmail.com>
2025-06-24 23:06:24 +0800

    Update LandingPage

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,60 +1,89 @@
-import "../styles/landing-page.css"
+import "../styles/landing-page.css";
+import illustration1 from "../assets/icons/illustration-1.webp";
 import { useNavigate } from "react-router-dom";
 import LoginPage from "./LoginPage";
 
 function LandingPage() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    function onClickLogin(){
-        navigate("/login");
-    }
-    function onClickSignIn(){
-        navigate("/register");
-    }
+  function onClickLogin() {
+    navigate("/login");
+  }
+  function onClickSignIn() {
+    navigate("/register");
+  }
 
-    return <>
-    <div className='header-container'>
-        <div className='header-row'>
-            <div className="logo-text">
-                NoteMe
-            </div>
-            <div>
-                <button>HOME</button>
-                <button>CONTACT</button>
-                <button>ABOUT US</button>
-            </div>
+  return (
+    <>
+      <div className="header-container">
+        <div className="header-row">
+          <div className="logo-text"></div>
+          <div>
+            <a className="header-button">HOME</a>
+            <a className="header-button">CONTACT</a>
+            <a className="header-button">ABOUT US</a>
+          </div>
         </div>
-        <div className="content-container">
-            <div className="content-text">
-                Start your task here!
-            </div>
-            <div className="content-button-container">
-                <button className="content-button" onClick={onClickLogin}>Login</button>
-                <button className="content-button" onClick={onClickSignIn}>Sign In</button>
-            </div>
+      </div>
+      <div className="content-container">
+        <div className="content-img-container">
+          <img className="content-img" src={illustration1} />
         </div>
-    </div>
-    <div className="feature-container">
-        <div className="feature-text">Features</div>
+        <div className="context-right-container">
+          <div>
+            <div className="content-text">Welcome back, User!</div>
+          </div>
+          <div className="content-button-container">
+            <div className="content-button-row-container">
+              <div className="content-button-text">Don't have an account?</div>
+              <button className="content-button" onClick={onClickSignIn}>
+                SIGN IN
+              </button>
+            </div>
+            <div className="content-button-row-container">
+              <div className="content-button-text">
+                Already have an account?
+              </div>
+              <button className="content-button" onClick={onClickLogin}>
+                LOGIN
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="feature-container">
+        <div className="feature-text">Web Development Foundation</div>
         <div className="feature-list-container">
-            <div className="feature-item-container">
-                <div className="feature-img"></div>
-                <div className="feature-item">Tasks</div>
-                <div>Text Description</div>
-            </div>
-            <div className="feature-item-container">
-                <div className="feature-img-2"></div>
-                <div className="feature-item">Checklist</div>
-                <div>Text Description</div>
-            </div>
-            <div className="feature-item-container">
-                <div className="feature-img-3"></div>
-                <div className="feature-item">History</div>
-                <div>Text Description</div>
-            </div>
+          <div className="feature-item-container">
+            <div className="feature-img html"></div>
+            <div className="feature-item-text">HTML</div>
+            <div className="feature-item-desc">for structure</div>
+          </div>
+          <div className="feature-item-container">
+            <div className="feature-img css"></div>
+            <div className="feature-item-text">CSS</div>
+            <div className="feature-item-desc">for styling</div>
+          </div>
+          <div className="feature-item-container">
+            <div className="feature-img js"></div>
+            <div className="feature-item-text">JavaScript</div>
+            <div className="feature-item-desc">for interactivity</div>
+          </div>
         </div>
-    </div>
+      </div>
+      <div className="message-container">
+        <div className="message-top-container">
+          <div className="message-text">Thank you for visiting our page!</div>
+        </div>
+        <div className="message-bottom-container"></div>
+      </div>
+      <div className="footer-container">
+        <div className="footer-text">
+          © 2025 MyWebpage. All rights reserved.
+        </div>
+      </div>
     </>
+  );
 }
 
 export default LandingPage;
